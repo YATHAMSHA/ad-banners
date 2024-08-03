53a1e425406d6836b96097e3abd4e3931485d967
YATHAM <yatham024@gmail.com>
2024-08-04 03:20:18 +0530

    Add existing project files to Git

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: '/ad-banner',
+    assetPrefix: '/ad-banner/',
+    trailingSlash: true // Optional: if you want to add a trailing slash to all URLs
+  };
+  
+  export default nextConfig;
+  
